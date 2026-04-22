eb35346789e0082665f81bc2e0e4d19f68807722
FKouhai <frandres00@gmail.com>
2026-04-22 21:11:11 +0200

    chore: own os exit 1 at main leve from cmd.Executre
    done after switching from Run: to RunE

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "trigo/cmd"
+import (
+	"fmt"
+	"os"
+
+	"trigo/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
