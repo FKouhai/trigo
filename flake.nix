3bbe126753fcaf510457706f15578a713ff9c4a3
FKouhai <frandres00@gmail.com>
2026-04-22 20:10:38 +0200

    chore: add nix stuff

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "trigo - a tree command replacement written in Go";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "trigo";
+            version = "0.1.0";
+            src = ./.;
+            vendorHash = "sha256-wzsBqDlKIkiKUAoHiDjicqEJ+8wflXZxDrdOIQoEWBE=";
+          };
+        }
+      );
+    };
+}
